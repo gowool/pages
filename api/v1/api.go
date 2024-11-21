c23291b0bfffee1961cda8c298bd81f3a523fdb7
Igor Agapie <igoragapie@gmail.com>
2024-11-21 22:34:18 +0100

    chore: remove func ErrorTransformer

--- api/v1/api.go
+++ api/v1/api.go
@@ -1,26 +1,5 @@
 package v1
 
-import (
-	"context"
-	"errors"
-
-	"github.com/danielgtaylor/huma/v2"
-	"github.com/gowool/echox/api"
-
-	"github.com/gowool/pages"
-)
+import "github.com/gowool/echox/api"
 
 var Info = api.CRUDInfo{Area: "admin", Version: "v1"}
-
-func ErrorTransformer(_ context.Context, err error) error {
-	var statusErr huma.StatusError
-	if errors.As(err, &statusErr) {
-		return statusErr
-	}
-
-	if pages.IsOneOfNotFound(err) {
-		return huma.Error404NotFound("Not Found", err)
-	}
-
-	return huma.Error500InternalServerError("Internal Server Error", err)
-}
